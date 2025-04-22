3562c09b6482d762f1427be1d8d4cbc6ed275b79
Armando <al02836763@tecmilenio.mx>
2025-04-21 19:21:45 -0600

    Se agregaron los datos

---
+++ app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: "#app",
+    data() {
+      return {
+        chuck: [
+          { value: "Chuck Norris puede saltar en paracaídas al espacio exterior." },
+          { value: "El principal producto de Chuck Norris es el dolor." },
+          { value: "Chuck Norris no lee libros. Los mira fijamente hasta que obtiene la información que quiere." },
+          { value: "El tiempo no espera a nadie. A menos que ese hombre sea Chuck Norris." },
+          { value: "Si escribes Chuck Norris en Scrabble, ganas. Para siempre." }
+        ]
+      };
+    }
+  });
+  
